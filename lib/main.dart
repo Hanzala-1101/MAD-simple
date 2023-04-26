304517b61f286d22ba8b715ac660ea08799604cf
Hanzala-1101 <ansarihanzala1101@gmail.com>
2023-04-26 20:54:11 +0530

    navigate to next page

--- lib/main.dart
+++ lib/main.dart
@@ -22,98 +22,28 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-  final url =
-      'https://images.unsplash.com/photo-1604537529428-15bcbeecfe4d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzcxODl8MXwxfHNlYXJjaHwxfHxtb3VudGFpbnxlbnwwfHx8fDE2ODI1MTUyMjY&ixlib=rb-4.0.3&q=80&w=400';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Image Gallery")),
-      body: Center(
-          child: Row(
-        children: [
-          Column(children: [
-            Image.asset('assets/mountain.jpeg', fit: BoxFit.cover),
-            const Text(
-              "Image from assets",
-              style: TextStyle(fontSize: 32),
-            )
-          ]),
-          Column(children: [
-            Image.network(url, fit: BoxFit.cover),
-            const Text(
-              "Image from url",
-              style: TextStyle(fontSize: 32),
-            )
-          ])
-        ],
-      )),
-      drawer: Drawers(),
-    );
+        appBar: AppBar(title: const Text("Image Gallery")),
+        body: Center(
+            child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const Second_Page()));
+          },
+          child: Text("second page"),
+        )));
   }
 }
 
-class Drawers extends StatelessWidget {
-  const Drawers({super.key});
-  final url =
-      'https://images.unsplash.com/photo-1604537529428-15bcbeecfe4d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzcxODl8MXwxfHNlYXJjaHwxfHxtb3VudGFpbnxlbnwwfHx8fDE2ODI1MTUyMjY&ixlib=rb-4.0.3&q=80&w=400';
+class Second_Page extends StatelessWidget {
+  const Second_Page({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            padding: EdgeInsets.zero,
-            child: UserAccountsDrawerHeader(
-              margin: EdgeInsets.zero,
-              accountName: Text("Account Name"),
-              accountEmail: Text("person@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(url),
-              ),
-            ),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.home,
-              color: Colors.black,
-            ),
-            title: Text(
-              "Home",
-              textScaleFactor: 1.2,
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.person_off,
-              color: Colors.black,
-            ),
-            title: Text(
-              "Profile",
-              textScaleFactor: 1.2,
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.mail,
-              color: Colors.black,
-            ),
-            title: Text(
-              "Email me",
-              textScaleFactor: 1.2,
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(title: const Text("Image Gallery")),
+        body: Center(child: Text("this is second page")));
   }
 }
