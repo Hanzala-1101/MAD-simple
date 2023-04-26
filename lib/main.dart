94de29f4983df82e5c434f6eaed554a2d5e3c94f
Hanzala-1101 <ansarihanzala1101@gmail.com>
2023-04-26 20:58:47 +0530

    main.dart code

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,119 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Image',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Flutter Form Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('EMPLOYEE REGISTRATION FORM'),
+        ),
+        body: MyForm(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyForm extends StatefulWidget {
+  @override
+  _MyFormState createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  String _email = "0";
+  String _password = "0";
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  void _submitForm() {
+    final FormState? form = _formKey.currentState;
+    if (!form!.validate()) {
+      showMessage('Form is not valid! Please review and correct.');
+    } else {
+      form.save(); //This invokes each onSaved event
+      showMessage('Email: $_email\n'
+          'Password: $_password\n');
+    }
+  }
+
+  void showMessage(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text("Dome app")),
-        body: Center(
-          child: Text(
-            "Hello world",
-            style: TextStyle(fontSize: 52, color: Colors.greenAccent),
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Employee Email ID',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your email address.';
+                  }
+                  if (!RegExp(r'\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$')
+                      .hasMatch(value)) {
+                    return 'Please enter a valid email address.';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _email = value!;
+                },
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a password.';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 characters long.';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _password = value!;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: const Text('Submit'),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
